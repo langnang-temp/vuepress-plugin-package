7445ae662c23c2602ac37d7a796442643a90b669
langnang <langnang.chen@outlook.com>
2023-02-17 21:13:27 +0800

    delete useless file

--- src/utils/request.js
+++
@@ -1,9 +0,0 @@
-const axios = require("axios");
-
-const request = axios.create({
-  baseURL: "https://some-domain.com/api/",
-  timeout: 1000,
-  headers: { "X-Custom-Header": "foobar" },
-});
-
-module.exports = request;
